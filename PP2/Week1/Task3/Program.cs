a507e1910e9858dc6aade231c3108d6a828b8216
MagiaGroz <grozaga@gmail.com>
2019-01-27 22:29:52 +0600

    1

---
+++ PP2/Week1/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // size of massive
+            int[] a = new int[n*2];
+           
+            string[] nums = Console.ReadLine().Split();  // take separate inetegers
+            for(int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(nums[i]);  // filling massive
+                a[i+1] = int.Parse(nums[i]);
+                Console.Write(a[i] + " " + a[i + 1]+" ");  // print result
+
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
