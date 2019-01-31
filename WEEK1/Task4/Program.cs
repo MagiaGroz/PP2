c223e41495a0d2f0316dd2264ce34b6b0d9fbaf2
MagiaGroz <grozaga@gmail.com>
2019-02-01 00:22:12 +0600

    ver3.0

--- WEEK1/Task4/Program.cs
+++ WEEK1/Task4/Program.cs
@@ -11,20 +11,32 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-
+            string[,] a = new string[n, n]; // создание 2д массива
             for (int i = 0; i < n; i++)
             {
-                int num = -1;  // берем -1 так как i начинается с нуля
+                int num = -1;  // берем -1 так как i начинается с нуля и если будет 0 то оно сразу остоновится
                 for (int j = 0; j < n; j++)
                 {
 
-                    Console.Write("[*]");
-                    num++;
-                    if (num == i) { break; }
+                    a[i,j]="[*]"; // заполнение 2д массива 
+                    num++; // следующая строка звезд всегда должна быть больше на 1
+                    if (num == i) { break; } // остонавливает заполнение массива
 
 
                 }
-                Console.WriteLine(" ");
+                
+            }
+            for (int i = 0; i < n; i++)
+            {
+               
+                for (int j = 0; j < n; j++)
+                {
+
+                    Console.Write(a[i, j]); // вывод массива
+
+
+                }
+                Console.WriteLine(" "); // вывести строки отдельно
             }
             Console.ReadKey();
         }
